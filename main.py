d4ead5b200946afc4023fb03742e107b55e5c332
Rodrigo Dias <rodrigo.pitta@gmail.com>
2018-09-02 16:26:50 +0100

    saving rs

--- main.py
+++ main.py
@@ -3,39 +3,64 @@ from bs4 import BeautifulSoup
 import os.path
 import hashlib
 import json
+import re
+
+
+
+def compose_url(url, search_term, location, page, increment):
+    search_term = "q=" + search_term
+    location = "&l=" + location 
+    page = '&start=' + str(page)
+    return url + "?" + search_term + location + page
+
+
+def get_from_cache(filename):
+    file_exists = os.path.isfile(cache_filename) 
+    if not file_exists:
+        response = requests.get(url)
+        with open(cache_folder + cache_filename, 'a') as cf:
+            cf.write(response.text)
+
+    with open(cache_folder + cache_filename, 'r') as cf:
+        response = cf.read()
+    
+    return response    
+
 
 url = "https://www.indeed.co.uk/jobs"
-search_term = "q=python"
-location = "&l=London"
 page = 0
 increment = 10
 cache_folder = './cache/'
 
-url = url + search_term + location + '&start=' + str(page)
+url = compose_url(url, "python", "london", page, increment)
 
-# cache
+# print("URl: " + url)
+
 cache_filename = hashlib.md5(url.encode('utf-8')).hexdigest()
-file_exists = os.path.isfile(cache_filename) 
-if not file_exists:
-    response = requests.get(url)
-    with open(cache_folder + cache_filename, 'a') as cf:
-        cf.write(response.text)
-
-with open(cache_folder + cache_filename, 'r') as cf:
-    response = cf.read()
+response = get_from_cache(cache_filename)
 
 soup = BeautifulSoup(response, 'html.parser')
 
 results = soup.select('div.result')
 
 
+# reset file if existed
+results_filename = 'results/' + cache_filename + ".txt"
+with open(results_filename, 'w+') as f:
+    f.close
 
-# "https://www.indeed.co.uk/company/Baaltek/jobs/Python-Developer-7edaca00286cff06?fccid=c2be16111c46764a&vjs=3"
-
-
+uniq_anchors = list()
 for k, result in enumerate(results):
     anchors = result.select('a[href]')
-    print(anchors)
+    for anchor in anchors:
+        href = anchor.attrs['href']
+        if str(href).endswith('&vjs=3'):
+            # if result is unique
+            if not any(href in s for s in uniq_anchors):
+                uniq_anchors.append(href)
+                with open(results_filename, 'a') as f:
+                    f.write(href+"\n")
+
     
 
 
