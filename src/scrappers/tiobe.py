3d2a15991ca86b01871d15ca68dcf710c25fec87
Rodrigo Dias <rodrigo.pitta@gmail.com>
2018-09-19 10:06:18 +0100

    better tiobe

--- src/scrappers/tiobe.py
+++ src/scrappers/tiobe.py
@@ -3,6 +3,8 @@ import os.path
 import hashlib
 from bs4 import BeautifulSoup
 from scrappers.scrapper import Scrapper
+import urllib
+import datetime
 
 class Tiobe(Scrapper):
 
@@ -14,33 +16,40 @@ class Tiobe(Scrapper):
         self.log.info("Running " + __name__)
         url = "https://www.tiobe.com/tiobe-index/"
         
-        cache_filename = hashlib.md5(url.encode('utf-8')).hexdigest()
+        # cache_filename = hashlib.md5(url.encode('utf-8')).hexdigest() + ".html"
+        cache_filename = 'tiobe-' + str(datetime.date.today()) + '.html'
         response = self.get_from_cache(cache_filename, url)
         
         soup = BeautifulSoup(response, 'html.parser')
-
         results = soup.select('table.table-striped')
         
-        results_filename = self.cache_results(cache_filename)
-        self.process_results(results, results_filename)
+        self.process_results(results)
 
 
-    def process_results(self, results, results_filename):
+    def process_results(self, results):
+        top_languages = []
         for k, result in enumerate(results):
             rows = result.select('tr')
             
             # remove the header
             header = rows.pop(0)
+            
             for row in rows:
                 info = row.find_all('td')
+                language = {
+                    'position': info[0].text,
+                    'name': info[3].text,
+                    'share': info[4].text,
+                    'monthly_movement': info[5].text,
+                }
+                
+                top_languages.append(language)
             
+        write_tiobe_to_file(top_languages)
+    
+    def write_tiobe_to_file(top_languages):
+        pass
             
-
-    def cache_results(self, cache_filename):
-        results_filename = './cache/' + cache_filename
-        with open(results_filename, 'w+') as f:
-            f.close
-        return results_filename
 
 
     def get_from_cache(self, cache_filename, url):
